8610c33cd6ebf935961c4e4b5409bbb2718ac8e1
Simon Prickett <simon@crudworks.org>
2024-01-25 23:23:01 +0000

    Now runs the query for simple point use case.

--- server.js
+++ server.js
@@ -24,6 +24,30 @@ app.get('/', async (req, res) => {
 // Expects the body to be a GeoJSON representation:
 // https://en.wikipedia.org/wiki/GeoJSON
 app.post('/search', async (req, res) => {
+  // Let's see what sort of request this is...
+  if (req.body.point) {
+    // Simple point.
+    const wktString = `POINT(${req.body.point.lng} ${req.body.point.lat})`;
+
+    try {
+      const results = await crate.execute(
+        'SELECT name, boundaries, forecast from shipping_forecast.regions WHERE WITHIN(?, boundaries) LIMIT 1',
+        [ wktString ]
+      );
+
+      console.log(JSON.stringify(results));
+    } catch(e) {
+      console.log(e);
+    }
+  } else if (req.body.polygon) {
+    // User supplied a search polygon.
+    const wktString = wellknown.stringify(req.body.polygon);
+    console.log(`SELECT * FROM shipping_forecast.regions WHERE INTERSECTS('${wktString}', boundaries);`);
+    // TODO run the query...
+  }
+
+  // TODO what about line string type?
+
   return res.json({
     todo: 'Need to write this!'
   });
